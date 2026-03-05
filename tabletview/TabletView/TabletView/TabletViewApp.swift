837eeb7e9cc75ab7f669eccb3b491462064c3115
Karson Mellott <karsonmellott@gmail.com>
2026-03-05 08:32:55 -0700

    chore: Added backend and iOS tabletView

---
+++ tabletview/TabletView/TabletView/TabletViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabletViewApp.swift
+//  TabletView
+//
+//  Created by Karson Mellott on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct TabletViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
